99373503821b650f786733d268a4c82d13af2bc5
MateoPI <mpiraola@gmail.com>
2021-06-17 23:40:08 -0300

    agregando css de estilos y registro.html

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.listen(3000, () => {
 });
 
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "/views/vistadedetalledeproducto.html"));
+    res.sendFile(path.join(__dirname, "/views/vistaderegistro.html"));
 });
