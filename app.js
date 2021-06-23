ce1ab4dbad177853c27e1d6cc68693abfd307f23
catalinabrianese <catalinabrianese@hotmail.com>
2021-06-23 12:08:19 -0300

    agregué las vistas desde index y accesibilidad desde la vista de producto al registro y login

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static(path.join(__dirname, "./views")));
 
 app.listen(3000, () => {
     console.log("server corriendo")
