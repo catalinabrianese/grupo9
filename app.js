c0b2ffb3139368315c899373a1d2f02c6fbc656b
catalinabrianese <catalinabrianese@hotmail.com>
2021-06-26 14:37:48 -0300

    edite el app.js para que funcione la ruta

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express= require("express");
 const app = express();
 const path = require("path");
-const mainRoutes = require("./src/routes/mainRoutes");
+const mainRouter = require("./src/routes/mainRoutes");
 
 app.use(express.static(path.join(__dirname, "./public")));
-
+app.use("/", mainRouter);
 app.set("view engine", "ejs");
 
 app.listen(3000, () => {
     console.log("server corriendo")
 });
 
-app.get("/", function(req, res){
-    res.sendFile((__dirname, "./views/index.ejs"));
-});
+
 
