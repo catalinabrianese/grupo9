9c93036cb62857873baa7bdb50543a30428b2a68
MateoPI <mpiraola@gmail.com>
2021-07-08 23:29:24 -0300

    cambios en vista detalle producto

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -9,7 +9,13 @@ module.exports={
         res.render('./users/vistaderegistro');
     },
     products:(req,res)=>{
-        res.render("./products/vistadedetalledeproducto", {products: productos});
+        let producto = null;
+        for (let i=0; i<productos.length; i++) {
+            if (productos[i].id == req.params.id){
+                producto = productos[i]
+            } 
+        }
+        res.render("./products/vistadedetalledeproducto", {producto: producto});
     },
     login:(req,res)=>{
         res.render("./users/vistadelogin");
